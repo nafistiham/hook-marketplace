e3da2d5706f627ea52b7993f722ebcfc3bf2dee0
Nafis Tiham <nafistiham@gmail.com>
2026-03-14 12:30:06 +0600

    chore: add varsIgnorePattern to eslint no-unused-vars for destructured discard vars

--- eslint.config.js
+++ eslint.config.js
@@ -15,7 +15,7 @@ export default [
     },
     rules: {
       '@typescript-eslint/no-explicit-any': 'error',
-      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
     },
   },
   {
